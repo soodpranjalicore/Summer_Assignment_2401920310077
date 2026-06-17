6bf0582f68fb09ea219d0ce050ca316b6440ee1a
Pranjali Sood <soodpranjali@gmail.com>
2026-06-17 22:58:03 +0530

    Week 3 Day 3 solutions

---
+++ Week_3/Day_3/MinStack.java
@@ -0,0 +1,35 @@
+public class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+        
+    }
+    
+    public void push(int value) {
+        stack.push(value);
+        if (minStack.isEmpty() || value <= minStack.peek()) {
+            minStack.push(value);
+        }
+        
+    }
+    
+    public void pop() {
+        int poppedValue = stack.pop();
+        if(poppedValue == minStack.peek()) {
+            minStack.pop();
+        }
+        
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
